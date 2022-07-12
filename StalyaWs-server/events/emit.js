4a221363967cdedb13e50864c6fc7322d16f719e
Se-Lyan <58882370+Se-Lyan@users.noreply.github.com>
2022-07-12 19:15:10 +0200

    Updated some text

--- StalyaWs-server/events/emit.js
+++ StalyaWs-server/events/emit.js
@@ -6,5 +6,5 @@
 exports.emit = function(socket, event_name){
   var data = Array.from(arguments)
   socket.write(JSON.stringify({event_name: event_name, data: data}))
-  console.log("data sended")
+  console.log("data sent")
 }
